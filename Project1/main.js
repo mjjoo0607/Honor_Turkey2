3bc8197f969ee5bf9fa7dd3aede992c711063b2e
ptapurva2008 <ptapurva2008@gmail.com>
2017-04-06 11:48:52 -0500

    Update main.js

--- Project1/main.js
+++ Project1/main.js
@@ -424,7 +424,7 @@ function cont_fb_intro() {
            },35000);*/
 	  
           setTimeout(function () {
-              alertify.success('Sam says: "Hi! nice to meet you"');
+              alertify.success('Sam says: "Hi! Nice to meet you"');
 		  //document.getElementById("bottomcomment").style.visibility = "visible";
               // $(".commentby").show('Georgeee commented "Hello! nice to meet you"');
           }, 30000);
