7509c456aa7b2e665236a6e9e41761234eaa7d3e
mjjoo0607 <minjoojoo67@gmail.com>
2017-04-17 22:00:09 -0500

    Update main.js

--- Project1/main.js
+++ Project1/main.js
@@ -23,8 +23,8 @@ $(function() {
 	// If the study is called with a parameter for redirection, as explained in the documentation, this value is overwritten. 
 	// To the redirect link, the following information will be appended: (1) participant number, (2) condition, (3) username, (4) description submitted by participant. These variables can be extracted from the link, saved as data, and used for linking the Social Media Ostracism paradigm to subsequent tasks and measures. See documentation for more details.
 
-    settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_78y5vvwkxJZrLtH';
-	 // settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_0rFAtUZh2zg7oA5';
+    settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_78y5vvwkxJZrLtH?id=%SURVEY_CODE%';
+	 // settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_0rFAtUZh2zg7oA5?id=%SURVEY_CODE%';
 
 	
 	// **Tasklength**     
